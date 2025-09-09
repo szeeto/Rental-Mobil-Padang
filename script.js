7d40c946dac7cc4525ca67dce0f606bf1cb7dd94
Szeeto <patrasawali93@gmail.com>
2025-09-09 13:33:14 +0700

    update

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-  const toggle = document.getElementById('navbarToggle');
-  const menu = document.querySelector('.menu ul');
-  toggle.addEventListener('click', function() {
-    menu.classList.toggle('active');
-  });
+
+const toggle = document.getElementById('navbarToggle');
+const menu = document.querySelector('.menu ul');
+toggle.addEventListener('click', function () {
+  menu.classList.toggle('active');
+});
+
+// Tutup menu saat klik link
+menu.addEventListener('click', function (e) {
+  if (e.target.tagName === 'A') {
+    menu.classList.remove('active');
+  }
+});
